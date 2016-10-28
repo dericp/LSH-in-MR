5dd7fa2db80c8a22b171bd41234d3ec09afa25ef
Deric Pang <pderichai@gmail.com>
2016-10-28 16:37:22 +0200

    Should actually be right now.

--- task1/near_duplicate_videos_map_reduce.py
+++ task1/near_duplicate_videos_map_reduce.py
@@ -1,69 +1,63 @@
 import numpy as np
 
-r = 50
-b = 20
-num_documents = 700
-
-
-def generate_hash_function(p=23081):
-    a = np.random.randint(1, np.iinfo(np.int64).max)
-    b = np.random.randint(np.iinfo(np.int64).max)
-
-    def hash_function(row_number):
-        return ((a * row_number + b) % p) % num_documents
-
-    return hash_function
-
-
-def min_hash_row(hash_function, row_numbers):
-    min_hash = np.iinfo(np.int64).max
-    for row in row_numbers:
-        hash = hash_function(row)
-        if hash < min_hash:
-            min_hash = hash
-
-    return min_hash
-
-sig_matrix_hash_functions = list()
-band_hash_functions = list()
-
-for i in range(0, r * b):
-    sig_matrix_hash_functions.append(generate_hash_function())
-
-for i in range(0, r * b):
-    band_hash_functions.append(generate_hash_function())
-
-sig_matrix_hash_functions = np.asarray(sig_matrix_hash_functions)
-band_hash_functions = np.asarray(band_hash_functions)
-
 
 def mapper(key, value):
-    # key: None
-    # value: one line of input file
+    r = 20
+    b = 50
+    n = 8192
+    p = 8209
+
     tokens = value.split()
-    doc_id = tokens[0]
-    row_numbers = map(int, tokens[1:])
-    sig_col = list()
+    video_id = int(tokens[0][6:])
+    shingles = map(int, tokens[1:])
+    sig_mat_col = list()
 
-    # generating signature matrix column for this document
-    for curr_hash_function in sig_matrix_hash_functions:
-        sig_col.append(min_hash_row(curr_hash_function, row_numbers))
+    np.random.seed(0)
 
-    # hash each "strip" in each band for this document
-    for band in range(0, b):
-        current_sum = 0
-        for row in range(0, r):
-            current_row = r * band + row
-            current_sum += band_hash_functions[current_row](sig_col[current_row])
-            #print 'current sum is ' + current_sum
+    for i in range(r * b):
+        a = np.random.randint(1, n)
+        x = np.random.randint(n)
+        assert type(a) is int
+        assert type(x) is int
+        min_hash = np.iinfo(np.int16).max
+        for shingle in shingles:
+            assert type(shingle) is int
+            curr_hash = ((a * shingle + x) % p) % n
+            if curr_hash < min_hash:
+                min_hash = curr_hash
 
-        yield(str(current_sum % num_documents) + '_band' + str(band), doc_id)
+        sig_mat_col.append(min_hash)
+
+    assert len(sig_mat_col) == r * b, 'len ' + str(len(sig_mat_col)) + ' r * b ' + str(r * b)
+
+    hash_sum = 0
+    np.random.seed(1)
+
+    for row_num in range(r * b):
+        band_num = row_num / r
+        a = np.random.randint(1, n)
+        x = np.random.randint(n)
+
+        hash_sum += ((a * sig_mat_col[row_num] + x) % p) % n
+
+        # if we reach the end of the band, yield a tuple
+        if (row_num + 1) % r == 0:
+            # print 'end of band ' + str(band_num)
+            band_hash = hash_sum % n
+            hash_sum = 0
+            yield(hash('band' + str(band_num) + 'hash' + str(band_hash)), value)
+
 
 def reducer(key, values):
-    # key: key from mapper used to aggregate
-    # values: list of all value for that key
     if len(values) > 1:
         values.sort()
-        for i in range(0, len(values)):
+        for i in range(len(values)):
             for j in range(i + 1, len(values)):
-                yield(int(values[i][6:]), int(values[j][6:]))
+                # compare the candidate pairs
+                tokens1 = values[i].split()
+                tokens2 = values[j].split()
+                shingles1 = set(map(int, tokens1[1:]))
+                shingles2 = set(map(int, tokens2[1:]))
+
+                if float(len(shingles1.intersection(shingles2))) / float(len(shingles1.union(shingles2))) > 0.85:
+                    yield(int(tokens1[0][6:]), int(tokens2[0][6:]))
